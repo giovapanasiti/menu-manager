0c1d1dc9c22a6a51d454a28d2b6a5027204db61e
Giovanni Panasiti <g.panasiti@enpam.it>
2016-12-28 16:52:35 +0100

    add seed

--- server/main.js
+++ server/main.js
@@ -2,4 +2,40 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if (Category.find().count() < 1) {
+    var category = [
+      {
+        nameIt: 'Antipasti',
+        nameFr: 'Entrée',
+        nameDe: 'Vorspeise',
+        nameEn: 'Starter',
+        categoryOrder: 1
+      },
+      {
+        nameIt: 'Primi Piatti',
+        nameFr: 'Premiers Cours',
+        nameDe: 'Erste Kurse',
+        nameEn: 'First Dishes',
+        categoryOrder: 2
+      },
+      {
+        nameIt: 'Secondi Piatti',
+        nameFr: 'Plat Principal',
+        nameDe: 'Hauptgericht',
+        nameEn: 'Second Dish',
+        categoryOrder: 2
+      },
+      {
+        nameIt: 'Dolci',
+        nameFr: 'Confiserie',
+        nameDe: 'Süßwaren',
+        nameEn: 'Dessert',
+        categoryOrder: 2
+      },
+    ];
+
+    category.forEach(function(category) {
+      Category.insert(category);
+    });
+  }
 });
