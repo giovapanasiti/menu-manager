8e27022a7d832550fc0ac88673cffb3328652a02
Giovanni Panasiti <g.panasiti@enpam.it>
2016-12-28 19:13:05 +0100

    TODO WIP PDF Multipage

--- client/templates/composer.js
+++ client/templates/composer.js
@@ -2,109 +2,6 @@ import '../plugins/_html2pdf.js';
 import '../plugins/html2canvas.js';
 import '../plugins/jspdf.min.js'
 
-function html2pdf (html,pdf,callback) {
-	var canvas = pdf.canvas;
-	if (!canvas) {
-		alert('jsPDF canvas plugin not installed');
-		return;
-	}
-	canvas.pdf = pdf;
-	pdf.annotations = {
-
-		_nameMap : [],
-
-		createAnnotation : function(href,bounds) {
-			var x = pdf.context2d._wrapX(bounds.left);
-			var y = pdf.context2d._wrapY(bounds.top);
-			var page = pdf.context2d._page(bounds.top);
-			var options;
-			var index = href.indexOf('#');
-			if (index >= 0) {
-				options = {
-					name : href.substring(index + 1)
-				};
-			} else {
-				options = {
-					url : href
-				};
-			}
-			pdf.link(x, y, bounds.right - bounds.left, bounds.bottom - bounds.top, options);
-		},
-
-		setName : function(name,bounds) {
-			var x = pdf.context2d._wrapX(bounds.left);
-			var y = pdf.context2d._wrapY(bounds.top);
-			var page = pdf.context2d._page(bounds.top);
-			this._nameMap[name] = {
-				page : page,
-				x : x,
-				y : y
-			};
-		}
-
-	};
-	canvas.annotations = pdf.annotations;
-
-	pdf.context2d._pageBreakAt = function(y) {
-		this.pageBreaks.push(y);
-	};
-
-	pdf.context2d._gotoPage = function(pageOneBased) {
-		while (pdf.internal.getNumberOfPages() < pageOneBased) {
-			pdf.addPage();
-		}
-		pdf.setPage(pageOneBased);
-	}
-
-	if (typeof html === 'string') {
-		// remove all scripts
-		html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
-
-		var iframe = document.createElement('iframe');
-		//iframe.style.width = canvas.width;
-		//iframe.src = "";
-		//iframe.document.domain =
-		document.body.appendChild(iframe);
-		var doc;
-		doc = iframe.contentDocument;
-		if (doc == undefined || doc == null) {
-			doc = iframe.contentWindow.document;
-		}
-		//iframe.setAttribute('style', 'position:absolute;right:0; top:0; bottom:0; height:100%; width:500px');
-
-		doc.open();
-		doc.write(html);
-		doc.close();
-
-		var promise = html2canvas(doc.body, {
-			canvas : canvas,
-			onrendered : function(canvas) {
-				if (callback) {
-					if (iframe) {
-						iframe.parentElement.removeChild(iframe);
-					}
-					callback(pdf);
-				}
-			}
-		});
-
-	} else {
-		var body = html;
-		var promise = html2canvas(body, {
-			canvas : canvas,
-			onrendered : function(canvas) {
-				if (callback) {
-					if (iframe) {
-						iframe.parentElement.removeChild(iframe);
-					}
-					callback(pdf);
-				}
-			}
-		});
-	}
-
-}
-
 
 Template.composer.helpers({
   categories: function(){
@@ -147,13 +44,16 @@ Template.composer.events({
       setTimeout(function () {
 
 
-        var pdf = new jsPDF('p', 'pt', 'letter');
+        var pdf = new jsPDF('p', 'pt', 'a4');
         console.log(pdf);
         // var canvas = document.getElementById('textarea');
         // console.log(canvas);
         // canvas.width = 72 * 8.5;
         // canvas.height = 72 * 11;
-        var testo = document.getElementById('textarea');
+        var testo = document.getElementById('menu-completo');
+        var pdfPart1 = document.getElementById('menu1');
+        var pdfPart2 = document.getElementById('menu2');
+        var pageHeight= pdf.internal.pageSize.height
 
         // html2pdf(testo, pdf, function(pdf){
         //   var iframe = document.createElement('iframe');
@@ -161,8 +61,9 @@ Template.composer.events({
         //   testo.appendChild(iframe);
         //   iframe.src = pdf.output('datauristring');
 	      //  });
-        pdf.addHTML(testo,function() {
-            pdf.output('datauri');
+        pdf.fromHTML( #qualcosa# ,function() {
+            // pdf.output('datauri');
+             pdf.output('web.pdf');
         });
 
         // html2canvas(testo, canvas, {
